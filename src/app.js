0e6203a55b504ffe0b7556ea8ea8326c3fca38c8
Wellington <wellingtongmb@gmail.com>
2024-11-04 14:20:22 -0300

    add mongodb in the project, make connection with mongo atlas and add mongoose to manager mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import connectDatabase from "./config/dbConnect.js";
+
+const connection = await connectDatabase();
+
+connection.on("error", (erro) => {
+  console.error("Connection error ", erro);
+});
+
+connection.once("open", () => {
+    console.log("Connection with database made successfully");
+})
 
 const app = express();
 app.use(express.json());
@@ -40,8 +51,14 @@ app.post("/books", (req, res) => {
 
 app.put("/books/:id", (req, res) => {
   const index = searchBook(req.params.id);
-    books[index].title = req.body.title;
-    res.status(200).json(books)
-})
+  books[index].title = req.body.title;
+  res.status(200).json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+  const index = searchBook(req.params.id);
+  books.splice(index, 1);
+  res.status(204).send("Book Removed");
+});
 
 export default app;
---
+++ src/config/dbConnect.js
@@ -0,0 +1,9 @@
+import mongoose, { mongo } from "mongoose";
+
+export default async function connectDatabase() {
+    mongoose.connect(
+        "mongodb+srv://admin:admin123456@cluster0.cvdcm.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    return mongoose.connection
+}
+
