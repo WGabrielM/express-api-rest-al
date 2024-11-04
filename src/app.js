84b082b24fd0dd7b5a0022923427affd1ac538f7
Wellington <wellingtongmb@gmail.com>
2024-11-04 10:53:19 -0300

    create post method and add new array with books

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const books = [
+  {
+    id: 1,
+    title: "Lord of Rings",
+  },
+  {
+    id: 2,
+    title: "The Hobbit",
+  },
+];
 
 app.get("/", (req, res) => {
   res.status(200).send("Node.js Tutorial");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
+});
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  res.status(201).send("Book Add Sucessfuly")
 });
 
 export default app;
