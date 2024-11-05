60cbb9641d395e8c5ef0aee69f19c97efbeb2ce6
Wellington <wellingtongmb@gmail.com>
2024-11-04 21:25:52 -0300

    create listBookById, updateBook and deleteBook controller

--- src/app.js
+++ src/app.js
@@ -15,22 +15,6 @@ connection.once("open", () => {
 const app = express();
 routes(app);
 
-app.get("/books/:id", (req, res) => {
-  const index = searchBook(req.params.id);
-  res.status(200).json(books[index]);
-});
-
-app.post("/books", (req, res) => {
-  books.push(req.body);
-  res.status(201).send("Book Add Sucessfuly");
-});
-
-app.put("/books/:id", (req, res) => {
-  const index = searchBook(req.params.id);
-  books[index].title = req.body.title;
-  res.status(200).json(books);
-});
-
 app.delete("/books/:id", (req, res) => {
   const index = searchBook(req.params.id);
   books.splice(index, 1);
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,8 +2,26 @@ import book from "../models/Book.js";
 
 class BookController {
   static async listBooks(req, res) {
-    const listBooks = await book.find({});
-    res.status(200).json(listBooks);
+    try {
+      const listBooks = await book.find({});
+      res.status(200).json(listBooks);
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - Request Fail` });
+    }
+  }
+
+  static async listBookById(req, res) {
+    try {
+      const id = req.params.id;
+      const bookFinded = await book.findById(id);
+      res.status(200).json(bookFinded);
+    } catch (error) {
+      if(req.params.id === null){
+        res.status(500).json({ message: `${error.message} - Book Not Found` });
+      } else {
+        res.status(500).json({ message: `${error.message} - Book Request Fail` });
+      }
+    }
   }
 
   static async addBook(req, res) {
@@ -14,6 +32,26 @@ class BookController {
       res
         .status(500)
         .json({ message: `${error.message} - Faill create new book` });
+    }
+  }
+
+  static async updateBook(req, res) {
+    try {
+      const id = req.params.id;
+      await book.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Book Updated" });
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - Faill update book` });
+    }
+  }
+
+  static async deleteBook(req, res) {
+    try {
+      const id = req.params.id;
+      await book.findByIdAndDelete(id);
+      res.status(200).json({ message: "Book Deleted" });
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - Faill delete book` });
     }
   }
 }
